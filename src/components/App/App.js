f8238e72f2855153c6c4a2949b23ff70f5f2d777
Ihor Kharchenko <ihor.stavr@gmail.com>
2022-08-07 00:22:24 +0300

    styled starting

---
+++ src/components/App/App.js
@@ -0,0 +1,29 @@
+import users from '../../user.json';
+import statisticalData from '../../data.json';
+import friends from '../../friends.json';
+import transactions from '../../transactions.json';
+import { Profile } from '../Profile/Profile';
+import { Statistics } from '../Statistics/Statistics';
+import { FriendList } from '../FriendList/FriendList';
+import { TransactionHistory } from '../TransactionHistory/TransactionHistory';
+import { Container } from './App.styled';
+
+export const App = () => {
+  return (
+    <Container>
+      {users.map(({ username, tag, location, avatar, stats }) => (
+        <Profile
+          key={username}
+          username={username}
+          tag={tag}
+          location={location}
+          avatar={avatar}
+          stats={stats}
+        />
+      ))}
+      <Statistics title={'Upload stats'} stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Container>
+  );
+};
